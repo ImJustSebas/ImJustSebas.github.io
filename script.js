adb8a57bae19cbb06cb7fb33cf9bf82fd6fb567c
Sebastian Porras Solano <97421288+ImJustSebas@users.noreply.github.com>
2024-04-26 14:42:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const sumar = document.getElementById('sum');
+const resta = document.getElementById('rest');
+const reinicio = document.getElementById('reset');
+
+
+sumar.onclick = function(){
+    var valueA = parseInt(document.getElementById('valueA').value);
+    var ValueB = parseInt(document.getElementById('valueB').value);
+    var result = document.getElementById('result');
+
+    var resultado = valueA + ValueB;
+
+    result.textContent = resultado;
+
+}
+
+resta.onclick = function(){
+    var valueA = document.getElementById('valueA').value;
+    var ValueB = document.getElementById('valueB').value;
+    var result = document.getElementById('result');
+
+    
+    var resultado = valueA - ValueB;
+
+    result.textContent = resultado;
+
+}
+
+reinicio.onclick = function(){
+    var result = document.getElementById('result');
+    var inputA = document.getElementById('valueA');
+    var inputB = document.getElementById('valueB');
+
+    inputB.value = "";
+    inputA.value = "";
+    result.textContent = "";
+}
